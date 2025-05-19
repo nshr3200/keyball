6c0e956e5f2b2d5411fdbd13d86c50903fa8eca2
nshr mstk <nshr3200@gmail.com>
2025-05-19 14:36:12 +0900

    fix compile error

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_nshr3200/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_nshr3200/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
 LT(_BSDL,KC_TAB),KC_A   , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_ENT   ,
     KC_LGUI  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RGUI  ,
-                          KC_NO    , KC_NO    , MO(_NUM) , MO(_SYM) , OS_CTL ,                   KC_RALT  ,  LT(_NAV,KC_SPC)     , XXXXXXX  , XXXXXXX  , TO(_GAME)
+                          KC_NO    , KC_NO    , MO(_NUM) , MO(_SYM) , KC_LCTL ,                   KC_RALT  ,  LT(_NAV,KC_SPC)     , XXXXXXX  , XXXXXXX  , TO(_GAME)
   ),
 
   [_SYM] = LAYOUT_universal(
@@ -56,7 +56,7 @@ LT(_BSDL,KC_TAB),KC_A   , KC_S     , KC_D     , KC_F     , KC_G     ,           
   [_NAV] = LAYOUT_universal(
     KC_NO    , KC_NO     , KC_NO    , KC_NO    , KC_NO    , KC_NO   ,                                        KC_HOME  , KC_PGDN  , KC_PGUP  , KC_END   , KC_NO    , KC_NO    ,
     KC_NO    , KC_RALT   , KC_LSFT  , KC_NO    , KC_NO    , KC_LGUI ,                                        KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , KC_NO    , KC_NO    ,
-    KC_NO    , KC_NO     , KC_NO    , OS_CTL   , KC_NO    , KC_NO   ,                                        KC_NO    , KC_LBRC  , KC_HASH  , KC_RBRC  , KC_NO    , KC_NO    ,
+    KC_NO    , KC_NO     , KC_NO    , KC_LCTL  , KC_NO    , KC_NO   ,                                        KC_NO    , KC_LBRC  , KC_HASH  , KC_RBRC  , KC_NO    , KC_NO    ,
                            KC_NO    , KC_NO    , KC_NO    , KC_NO   , KC_NO    ,                 KC_NO    ,  XXXXXXX             , XXXXXXX  , XXXXXXX  , XXXXXXX
   ),
 
