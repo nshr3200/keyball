421e1e09ebe054a70bf3143667467add69e46ee3
nshr mstk <nshr3200@gmail.com>
2025-06-03 09:25:29 +0900

    slash hold -> mouse layer

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_nshr3200/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_nshr3200/keymap.c
@@ -130,8 +130,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [_BASE] = LAYOUT_universal(
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_NO    ,
-LT(_BSDL,KC_TAB),KC_A   , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , TD(TD_LAYER_SHIFT)  , KC_ENT ,
-    KC_LGUI  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , MO(_SYM) ,
+LT(_BSDL,KC_TAB),KC_A   , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , TD(TD_LAYER_SHIFT)  , KC_ENT  ,
+    KC_LGUI  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , LT(_BSDL,C_SLSH)    , KC_RGUI ,
                           KC_NO    , KC_NO    , MO(_NUM) , MO(_SYM) , OS_CTL ,                    KC_RALT  , LT(_NAV, KC_SPC)    , XXXXXXX  , XXXXXXX  , TO(_GAME)
   ),
 
@@ -171,7 +171,6 @@ LT(_BSDL,KC_TAB),KC_A   , KC_S     , KC_D     , KC_F     , KC_G     ,           
   )
 };
 // clang-format on
-
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
