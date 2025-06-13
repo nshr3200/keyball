1ce15332ee3ba8b9608f9a22e6ec4642b037bff7
nshr mstk <nshr3200@gmail.com>
2025-06-13 21:51:44 +0900

    one shot shift.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_nshr3200/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_nshr3200/keymap.c
@@ -41,6 +41,7 @@ enum custom_keycodes {
     OS_MAC = SAFE_RANGE,
     OS_WIN,
     OS_CTL,
+    OS_SHIFT
 };
 
 const uint16_t PROGMEM comm_dot[] = {KC_COMM, KC_DOT, COMBO_END};
@@ -61,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 LT(_AML,KC_ESC),KC_Q    , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_NO    ,
 LT(_NAV,KC_TAB),KC_A    , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_ENT   ,
     KC_LGUI  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , LT(_NUM,KC_SLSH)    , KC_RGUI ,
-                          KC_NO    , KC_NO    , KC_RALT  , MO(_SYM) , OS_CTL   ,                  KC_RSFT  , KC_SPC              , XXXXXXX  , XXXXXXX  , MO(_NAV)
+                          KC_NO    , KC_NO    , KC_RALT  , MO(_SYM) , OS_CTL   ,                  OS_SHIFT , KC_SPC              , XXXXXXX  , XXXXXXX  , MO(_NAV)
   ),
 
   [_AML] = LAYOUT_universal(
@@ -75,28 +76,28 @@ LT(_NAV,KC_TAB),KC_A    , KC_S     , KC_D     , KC_F     , KC_G     ,           
     KC_NO    , KC_0     , KC_7     , KC_8     , KC_9     , KC_ASTR  ,                                        KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,
     KC_NO    , KC_0     , KC_4     , KC_5     , KC_6     , KC_PLUS  ,                                        KC_NO    , KC_NO    , KC_NO    , KC_NO    , XXXXXXX  , KC_NO    ,
     KC_NO    , KC_0     , KC_1     , KC_2     , KC_3     , KC_MINS  ,                                        KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,
-                          KC_COMM  , KC_DOT   , KC_RALT  , MO(_SYM) , OS_CTL   ,                  KC_RSFT  , KC_SPC              , XXXXXXX  , XXXXXXX  , KC_NO
+                          KC_COMM  , KC_DOT   , KC_RALT  , MO(_SYM) , OS_CTL   ,                  OS_SHIFT , KC_SPC              , XXXXXXX  , XXXXXXX  , KC_NO
   ),
 
   [_SYM] = LAYOUT_universal(
     KC_NO    , KC_HASH  , KC_CIRC  , KC_ASTR  , KC_LCBR  , KC_QUOT  ,                                        KC_DQUO  , KC_RCBR  , KC_AMPR  , KC_DLR   , KC_BSLS  , KC_COLN  ,
     MO(_SYM) , KC_PERC  , KC_EXLM  , KC_EQL   , KC_LPRN  , KC_PLUS  ,                                        KC_ENT   , KC_RPRN  , KC_COLN  , KC_QUES  , KC_SCLN  , KC_ENT   ,
     KC_LGUI  , KC_LABK  , KC_PIPE  , KC_MINS  , KC_RABK  , KC_AT    ,                                        KC_GRV   , KC_LBRC  , KC_COMM  , KC_DOT   , KC_RBRC  , KC_NO    ,
-                          OS_WIN   , OS_MAC   , KC_RALT  , XXXXXXX  , OS_CTL   ,                  KC_RSFT  , KC_SPC              , XXXXXXX  , XXXXXXX  , AML_TO
+                          OS_WIN   , OS_MAC   , KC_RALT  , XXXXXXX  , OS_CTL   ,                  OS_SHIFT , KC_SPC              , XXXXXXX  , XXXXXXX  , AML_TO
   ),
 
   [_NAV] = LAYOUT_universal(
     KC_NO    , KC_NO     , KC_NO    , KC_NO    , KC_NO    , KC_NO   ,                                        KC_HOME  , KC_PGDN  , KC_PGUP  , KC_END   , KC_NO    , KC_NO    ,
     KC_NO    , KC_NO     , KC_NO    , KC_NO    , KC_LGUI  , KC_NO   ,                                        KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , KC_NO    , KC_NO    ,
     KC_NO    , KC_NO     , KC_NO    , KC_NO    , KC_NO    , KC_NO   ,                                        KC_BSPC  , KC_BSPC  , KC_BSPC  , KC_DEL   , KC_NO    , KC_NO    ,
-                           KC_NO    , KC_NO    , KC_RALT  , MO(_SYM) , OS_CTL   ,                 KC_RSFT  , KC_NO               , XXXXXXX  , XXXXXXX  , KC_NO
+                           KC_NO    , KC_NO    , KC_RALT  , MO(_SYM) , OS_CTL   ,                 OS_SHIFT , KC_NO               , XXXXXXX  , XXXXXXX  , KC_NO
   ),
 
   [_GAME] = LAYOUT_universal(
     KC_NO    , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_NO    ,
     KC_NO    , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_NO    ,
     KC_NO    , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_NO    ,
-                          TO(_BASE), TO(_BASE), KC_RALT  , OS_CTL   , OS_CTL  ,                  KC_RSFT  , KC_SPC              , XXXXXXX  , XXXXXXX  , KC_NO
+                          TO(_BASE), TO(_BASE), KC_RALT  , OS_CTL   , OS_CTL  ,                   OS_SHIFT , KC_SPC              , XXXXXXX  , XXXXXXX  , KC_NO
   )
 };
 // clang-format on
@@ -135,11 +136,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false;
+        case OS_SHIFT:
+            if (record->event.pressed) {
+                is_os_shift_held = true;
+                os_shift_timer = timer_read();
+            } else {
+                if (is_os_shift_held && timer_elapsed(os_shift_timer) < TAPPING_TERM) {
+                    // タップ：ワンショットシフトを設定
+                    set_oneshot_mods(MOD_BIT(KC_LSFT));
+                } else {
+                    // ホールドだったので通常のShiftを解除
+                    unregister_mods(MOD_BIT(KC_LSFT));
+                }
+                is_os_shift_held = false;
+            }
+            return false; // OS_SHIFTを他に送信しない
         default:
             return true;
     }
 }
 
+void matrix_scan_user(void) {
+    if (is_os_shift_held && timer_elapsed(os_shift_timer) >= TAPPING_TERM) {
+        // ホールドと判定されたら、Shiftを有効化
+        register_mods(MOD_BIT(KC_LSFT));
+        is_os_shift_held = false;
+    }
+}
 
 #ifdef OLED_ENABLE
 
